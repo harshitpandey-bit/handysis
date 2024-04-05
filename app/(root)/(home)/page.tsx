09c99c21bad2ca9a6bc1ef23347ff87911a1d3c4
Anirban <anirbansharma36@gmail.com>
2024-04-05 19:53:35 +0530

    05/04/2024 -  [updated meeting typelist  page ](#10)

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -3,7 +3,7 @@ import MeetingTypeList from '@/components/MeetingTypeList';
 const Home = () => {
   const now = new Date();
 
-  //const time = now.toLocaleTimeString('en-IN', { hour: '2-digit', minute: '2-digit' });
+
   const date = (new Intl.DateTimeFormat('en-IN', { dateStyle: 'full' })).format(now);
 
   return (
